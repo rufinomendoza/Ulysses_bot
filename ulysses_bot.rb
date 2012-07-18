060b004a8d339a20df14de81e83bfdde08e0f14a
rufinomendoza <ThinkPadT400@Mobile>
2012-07-18 17:03:34 -0400

    Started off first triplet as well as method for moving to next duplet

--- ulysses_bot.rb
+++ ulysses_bot.rb
@@ -1,4 +1,13 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 source_text = <<EOF
+On his wise shoulders through the checkerwork of leaves the sun flung spangles, dancing coins.
+Ineluctable modality of the visible: at least that if no more, thought through my eyes. Signatures of all things I am here to read, seaspawn and seawrack, the nearing tide, that rusty boot. Snotgreen, bluesilver, rust: coloured signs. Limits of the diaphane. But he adds: in bodies. Then he was aware of them bodies before of them coloured. How? By knocking his sconce against them, sure. Go easy. Bald he was and a millionaire, maestro di color che sanno. Limit of the diaphane in. Why in? Diaphane, adiaphane. If you can put your five fingers through it it is a gate, if not a door. Shut your eyes and see.
+Stephen closed his eyes to hear his boots crush crackling wrack and shells. You are walking through it howsomever. I am, a stride at a time. A very short space of time through very short times of space. Five, six: the nacheinander. Exactly: and that is the ineluctable modality of the audible. Open your eyes. No. Jesus! If I fell over a cliff that beetles o'er his base, fell through the nebeneinander ineluctably! I am getting on nicely in the dark. My ash sword hangs at my side. Tap with it: they do. My two feet in his boots are at the ends of his legs, nebeneinander. Sounds solid: made by the mallet of Los Demiurgos. Am I walking into eternity along Sandymount strand? Crush, crack, crick, crick. Wild sea money. Dominie Deasy kens them a'. Won't you come to Sandymount, Madeline the mare?
+Rhythm begins, you see. I hear. Acatalectic tetrameter of iambs marching. No, agallop: deline the mare.
+Open your eyes now. I will. One moment. Has all vanished since? If I open and am for ever in the black adiaphane. Basta! I will see if I can see.
+See now. There all the time without you: and ever shall be, world without end.
 They came down the steps from Leahy's terrace prudently, Frauenzimmer: and down the shelving shore flabbily, their splayed feet sinking in the silted sand. Like me, like Algy, coming down to our mighty mother. Number one swung lourdily her midwife's bag, the other's gamp poked in the beach. From the liberties, out for the day. Mrs Florence MacCabe, relict of the late Patk MacCabe, deeply lamented, of Bride Street. One of her sisterhood lugged me squealing into life. Creation from nothing. What has she in the bag? A misbirth with a trailing navelcord, hushed in ruddy wool. The cords of all link back, strandentwining cable of all flesh. That is why mystic monks. Will you be as gods? Gaze in your omphalos. Hello! Kinch here. Put me on to Edenville. Aleph, alpha: nought, nought, one.
 Spouse and helpmate of Adam Kadmon: Heva, naked Eve. She had no navel. Gaze. Belly without blemish, bulging big, a buckler of taut vellum, no, whiteheaped corn, orient and immortal, standing from everlasting to everlasting. Womb of sin.
 Wombed in sin darkness I was too, made not begotten. By them, the man with my voice and my eyes and a ghostwoman with ashes on her breath. They clasped and sundered, did the coupler's will. From before the ages He willed me and now may not will me away or ever. A lex eterna stays about Him. Is that then the divine substance wherein Father and Son are consubstantial? Where is poor dear Arius to try conclusions? Warring his life long upon the contransmagnificandjewbangtantiality. Illstarred heresiarch' In a Greek watercloset he breathed his last: euthanasia. With beaded mitre and with crozier, stalled upon his throne, widower of a widowed see, with upstiffed omophorion, with clotted hinderparts.
@@ -6,6 +15,90 @@ Airs romped round him, nipping and eager airs. They are coming, waves. The white
 I mustn't forget his letter for the press. And after? The Ship, half twelve. By the way go easy with that money like a good young imbecile.
 Yes, I must.
 His pace slackened. Here. Am I going to aunt Sara's or not? My consubstantial father's voice. Did you see anything of your artist brother Stephen lately? No? Sure he's not down in Strasburg terrace with his aunt Sally? Couldn't he fly a bit higher than that, eh? And and and and tell us, Stephen, how is uncle Si? O, weeping God, the things I married into! De boys up in de hayloft. The drunken little costdrawer and his brother, the cornet player. Highly respectable gondoliers! And skeweyed Walter sirring his father, no less! Sir. Yes, sir. No, sir. Jesus wept: and no wonder, by Christ!
+I pull the wheezy bell of their shuttered cottage: and wait. They take me for a dun, peer out from a coign of vantage.
+—It's Stephen, sir.
+
+—Let him in. Let Stephen in.
+
+A bolt drawn back and Walter welcomes me.
+
+—We thought you were someone else.
+
+In his broad bed nuncle Richie, pillowed and blanketed, extends over the hillock of his knees a sturdy forearm. Cleanchested. He has washed the upper moiety.
+
+—Morrow, nephew.
+
+He lays aside the lapboard whereon he drafts his bills of costs for the eyes of master Goff and master Shapland Tandy, filing consents and common searches and a writ of Duces Tecum. A bogoak frame over his bald head: Wilde's Requiescat. The drone of his misleading whistle brings Walter back.
+
+—Yes, sir?
+
+—Malt for Richie and Stephen, tell mother. Where is she?
+
+—Bathing Crissie, sir.
+
+Papa's little bedpal. Lump of love.
+
+—No, uncle Richie...
+
+—Call me Richie. Damn your lithia water. It lowers. Whusky!
+
+—Uncle Richie, really...
+
+—Sit down or by the law Harry I'll knock you down.
+
+Walter squints vainly for a chair.
+
+—He has nothing to sit down on, sir.
+
+—He has nowhere to put it, you mug. Bring in our chippendale chair. Would you like a bite of something? None of your damned lawdeedaw airs here. The rich of a rasher fried with a herring? Sure? So much the better. We have nothing in the house but backache pills.
+
+All'erta!
+
+He drones bars of Ferrando's aria di sortita. The grandest number, Stephen, in the whole opera. Listen.
+
+His tuneful whistle sounds again, finely shaded, with rushes of the air, his fists bigdrumming on his padded knees.
+
+This wind is sweeter.
+
+Houses of decay, mine, his and all. You told the Clongowes gentry you had an uncle a judge and an uncle a general in the army. Come out of them, Stephen. Beauty is not there. Nor in the stagnant bay of Marsh's library where you read the fading prophecies of Joachim Abbas. For whom? The hundredheaded rabble of the cathedral close. A hater of his kind ran from them to the wood of madness, his mane foaming in the moon, his eyeballs stars. Houyhnhnm, horsenostrilled. The oval equine faces, Temple, Buck Mulligan, Foxy Campbell, Lanternjaws. Abbas father,—furious dean, what offence laid fire to their brains? Paff! Descende, calve, ut ne amplius decalveris. A garland of grey hair on his comminated head see him me clambering down to the footpace (descende!), clutching a monstrance, basiliskeyed. Get down, baldpoll! A choir gives back menace and echo, assisting about the altar's horns, the snorted Latin of jackpriests moving burly in their albs, tonsured and oiled and gelded, fat with the fat of kidneys of wheat.
+
+And at the same instant perhaps a priest round the corner is elevating it. Dringdring! And two streets off another locking it into a pyx. Dringadring! And in a ladychapel another taking housel all to his own cheek. Dringdring! Down, up, forward, back. Dan Occam thought of that, invincible doctor. A misty English morning the imp hypostasis tickled his brain. Bringing his host down and kneeling he heard twine with his second bell the first bell in the transept (he is lifting his) and, rising, heard (now I am lifting) their two bells (he is kneeling) twang in diphthong.
+
+Cousin Stephen, you will never be a saint. Isle of saints. You were awfully holy, weren't you? You prayed to the Blessed Virgin that you might not have a red nose. You prayed to the devil in Serpentine avenue that the fubsy widow in front might lift her clothes still more from the wet street. O si, certo! Sell your soul for that, do, dyed rags pinned round a squaw. More tell me, more still!! On the top of the Howth tram alone crying to the rain: Naked women! naked women! What about that, eh?
+
+What about what? What else were they invented for?
+
+Reading two pages apiece of seven books every night, eh? I was young. You bowed to yourself in the mirror, stepping forward to applause earnestly, striking face. Hurray for the Goddamned idiot! Hray! No-one saw: tell no-one. Books you were going to write with letters for titles. Have you read his F? O yes, but I prefer Q. Yes, but W is wonderful. O yes, W. Remember your epiphanies written on green oval leaves, deeply deep, copies to be sent if you died to all the great libraries of the world, including Alexandria? Someone was to read them there after a few thousand years, a mahamanvantara. Pico della Mirandola like. Ay, very like a whale. When one reads these strange pages of one long gone one feels that one is at one with one who once...
+
+The grainy sand had gone from under his feet. His boots trod again a damp crackling mast, razorshells, squeaking pebbles, that on the unnumbered pebbles beats, wood sieved by the shipworm, lost Armada. Unwholesome sandflats waited to suck his treading soles, breathing upward sewage breath, a pocket of seaweed smouldered in seafire under a midden of man's ashes. He coasted them, walking warily. A porterbottle stood up, stogged to its waist, in the cakey sand dough. A sentinel: isle of dreadful thirst. Broken hoops on the shore; at the land a maze of dark cunning nets; farther away chalkscrawled backdoors and on the higher beach a dryingline with two crucified shirts. Ringsend: wigwams of brown steersmen and master mariners. Human shells.
+
+He halted. I have passed the way to aunt Sara's. Am I not going there? Seems not. No-one about. He turned northeast and crossed the firmer sand towards the Pigeonhouse.
+
+—Qui vous a mis dans cette fichue position?
+
+—c'est le pigeon, Joseph.
+
+Patrice, home on furlough, lapped warm milk with me in the bar MacMahon. Son of the wild goose, Kevin Egan of Paris. My father's a bird, he lapped the sweet lait chaud with pink young tongue, plump bunny's face. Lap, lapin. He hopes to win in the gros lots. About the nature of women he read in Michelet. But he must send me La Vie de Jesus by M. Leo Taxil. Lent it to his friend.
+
+—C'est tordant, vous savez. Moi, je suis socialiste. Je ne crois pas en l'existence de Dieu. Faut pas le dire a mon p-re.
+
+—Il croit?
+
+—Mon pere, oui.
+
+Schluss. He laps.
+
+My Latin quarter hat. God, we simply must dress the character. I want puce gloves. You were a student, weren't you? Of what in the other devil's name? Paysayenn. P. C. N., you know: physiques, chimiques et naturelles. Aha. Eating your groatsworth of mou en civet, fleshpots of Egypt, elbowed by belching cabmen. Just say in the most natural tone: when I was in Paris; boul' Mich', I used to. Yes, used to carry punched tickets to prove an alibi if they arrested you for murder somewhere. Justice. On the night of the seventeenth of February 1904 the prisoner was seen by two witnesses. Other fellow did it: other me. Hat, tie, overcoat, nose. Lui, c'est moi. You seem to have enjoyed yourself.
+
+Proudly walking. Whom were you trying to walk like? Forget: a dispossessed. With mother's money order, eight shillings, the banging door of the post office slammed in your face by the usher. Hunger toothache. Encore deux minutes. Look clock. Must get. Ferme. Hired dog! Shoot him to bloody bits with a bang shotgun, bits man spattered walls all brass buttons. Bits all khrrrrklak in place clack back. Not hurt? O, that's all right. Shake hands. See what I meant, see? O, that's all right. Shake a shake. O, that's all only all right.
+
+You were going to do wonders, what? Missionary to Europe after fiery Columbanus. Fiacre and Scotus on their creepystools in heaven spilt from their pintpots, loudlatinlaughing: Euge! Euge! Pretending to speak broken English as you dragged your valise, porter threepence, across the slimy pier at Newhaven. Comment? Rich booty you brought back; Le Tutu, five tattered numbers of Pantalon Blanc et Culotte Rouge; a blue French telegram, curiosity to show:
+
+—Mother dying come home father.
+
+The aunt thinks you killed your mother. That's why she won't.
+
+
 EOF
 
 source_text_words = source_text.split(' ')
@@ -21,8 +114,44 @@ source_text_words.each_with_index do |word, index|
   end
 end
 
-puts word_pairs_and_probabilities
+first_key = []
+first_key << "of all"
+first_key << word_pairs_and_probabilities["of all"].sample
+first_triplet = first_key.join(' ').split(' ')
 
+puts first_triplet.join(' ')
+
+next_chain = first_triplet[1] + ' ' + first_triplet[2]
+
+def continue(argument, thing)
+  foo_key = []
+  foo_key << thing
+  foo_key << argument[thing].sample
+  foo_phrase = foo_key.join(' ').split(' ')
+  puts foo_phrase.join(' ')
+  iterative_chain = foo_phrase[-2] + ' ' + foo_phrase[-1]
+  return iterative_chain
+end
+
+zarg = continue(word_pairs_and_probabilities, next_chain)
+
+10.times do
+  continue(word_pairs_and_probabilities, zarg)
+end
+
+#initial_triplet = []
+#initial_triplet << word_pairs_and_probabilities.keys[0]
+#initial_triplet << probable_following_words
+
+#puts initial_triplet.to_s
+
+#third_word = word_pairs_and_probabilities[probable_following_words]
+
+#puts "#{probable_following_words} #{next_chain}"
+
+#word_pairs_and_probabilities.keys.each do |keys|
+#  puts "#{keys}"
+#end
 
 # simple_hash = {}
 # simple_hash["they came"] =["down"]
