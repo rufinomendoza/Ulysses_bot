ffcc1c2ea286d4b93235d63f4499960c98120687
rufinomendoza <ThinkPadT400@Mobile>
2012-07-23 15:06:11 -0400

    Added classes.  Still need to work on processor hold up.

---
+++ ulysses_bot.rb
@@ -0,0 +1,86 @@
+class LitBot
+  def eat_file(file_name)
+    file = File.open(file_name)
+    @source_text = file.read
+    file.close
+  end
+
+  def speak_first_10_words
+    puts @source_text.split(' ').first(10)
+  end
+
+  def digest_file
+    source_text_words = @source_text.split(' ')
+    @word_pairs_and_probabilities = {}
+
+    source_text_words.each_with_index do |word, index|
+      hash_key = "#{word} #{source_text_words[index + 1]}"
+      hash_value = source_text_words[index + 2]
+      if @word_pairs_and_probabilities[hash_key]
+        @word_pairs_and_probabilities[hash_key] << hash_value
+      else
+        @word_pairs_and_probabilities[hash_key] = [hash_value]
+      end
+    end
+  end
+
+  def markov
+    output_text = []
+    output_text << @word_pairs_and_probabilities.keys.sample.split(' ')
+
+    puts output_text.join(' ')
+
+    story = 0
+    while story < 10 do
+      word_pair = output_text.last(2).join(' ')
+      next_word = @word_pairs_and_probabilities[word_pair].sample unless @word_pairs_and_probabilities[word_pair].nil?
+      output_text << next_word
+
+      if next_word && (next_word.include?(".") || next_word.include?("?"))
+        story += 1
+      end
+    end
+    puts output_text.join(' ')
+  end
+
+  def speak
+    puts @word_pairs_and_probabilities.first
+  end
+end
+
+bender = LitBot.new
+bender.eat_file("huckle.txt")
+bender.speak_first_10_words # an example call
+bender.digest_file
+bender.markov
+bender.speak
+
+ulysses_bot = LitBot.new
+ulysses_bot.eat_file("ulysses.txt")
+ulysses_bot.speak_first_10_words # an example call
+ulysses_bot.digest_file
+ulysses_bot.markov
+ulysses_bot.speak
+
+# BONUS HW
+# hybrid_bot = ulysses_bot + bender
+# hybrid_bot.speak
+
+# ulysses = File.open("ulysses.txt")
+# source_text = ulysses.read
+# ulysses.close
+
+# source_text_words = source_text.split(' ')
+# word_pairs_and_probabilities = {}
+
+# source_text_words.each_with_index do |word, index|
+  # hash_key = "#{word} #{source_text_words[index + 1]}"
+  # hash_value = source_text_words[index + 2]
+  # if word_pairs_and_probabilities[hash_key]
+    # word_pairs_and_probabilities[hash_key] << hash_value
+  # else
+    # word_pairs_and_probabilities[hash_key] = [hash_value]
+  # end
+# end
+
+# # puts word_pairs_and_probabilities
